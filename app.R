a25decfae49eeff6c07f250358e4436878672511
Alyssa Obermayer <59879286+aobermayer4@users.noreply.github.com>
2023-05-05 12:26:36 -0400

    Add files via upload

---
+++ app.R
@@ -0,0 +1,467 @@
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+library(plotly) #install.packages("plotly")
+library(colourpicker) #install.packages("colourpicker")
+library(ggpubr)
+library(readxl)
+library(viridis)
+library(patchwork)
+library(shinycssloaders)
+library(shinyjqui)
+library(pheatmap)
+library(EnhancedVolcano)
+library(data.table)
+library(reshape2)
+library(stringr)
+
+
+####----Input Files----####
+
+## Volcano File
+L29_Vitro_Diff <- read.delim("L29_vitro_Control_vs_knockdown_diff.txt")
+L29_Vitro_Diff <- mutate(L29_Vitro_Diff, log_pval = -log10(L29_Vitro_Diff$P.Value))
+L29_Vitro_Diff <- mutate(L29_Vitro_Diff, log_adjpval = -log10(L29_Vitro_Diff$adj.P.Val))
+volGenes <- L29_Vitro_Diff[,1]
+
+
+## Mito File
+GEX5<- read.table(file="GEX5Line.txt", header=T)
+###descriptive
+grouped <- group_by(GEX5, position)
+combo<-summarise(grouped, mean=mean(uniq_cell_umiN), sd=sd(uniq_cell_umiN))
+
+## Violin File
+expr<- read.delim(file="TCGA_CHOL_Expression_PatientID.txt", header=T, check.names=F) 
+meta<- read.delim(file="TCGA_CHOL_Clinical_PatientID.txt", header=T, check.names=F) 
+GeneList <- expr[,1]
+ColumnList <- colnames(meta)
+
+## Heatmap
+load("MS_2.rda")
+heatmap_df <- df
+rm(df)
+sample_anno <- sample_meta
+sample_anno_col <- "sampleLabel"
+feature_anno <- feature_meta
+feature_anno_col <- "featureName"
+
+## Histogram
+histogram_df <- unlist(df)
+
+## DRM Data
+DMR_data <- fread("autosomes.beta.txt.sorted.chr16.txt")
+
+
+####----Volcano----####
+
+module_volcano_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    sidebarPanel(
+      colourInput(NS(id,"dotcol"),"Pick point color", value = "black"),
+      radioButtons(NS(id,"stat"),"Choose statistic for plot",
+                   choices=c("-Log10 P value" = "log_pval","-Log10 adjusted P value" = "log_adjpval"),
+                   selected = "log_pval")
+    ),
+    mainPanel(
+      plotOutput(NS(id,"volcano"))
+    )
+  )
+}
+
+module_volcano_server <- function(id, data) {
+  moduleServer(id, function(input, output, session) {
+    output$volcano <- renderPlot({
+      ggplot(data, aes(x=logFC, y=data[,input$stat])) + geom_point(col=input$dotcol)
+    })
+  })
+}
+
+
+####----Volcano - New----####
+
+##--Plot Volcano--##
+
+plotVolcano <- function(res, feature_col = NULL, padj_col = NULL, log2fc_col = NULL,
+                        fdr = 0.05, log2fc = 1, TopHits = 10) {
+  
+  require(dplyr)
+  require(ggplot2)
+  
+  # identify column names for feature_col, padj and log2fc
+  if (!is.null(feature_col)) feature_col <- feature_col else feature_col <- "variable"
+  if (!is.null(padj_col)) padj_col <- padj_col else padj_col <- "padj"
+  if (!is.null(log2fc_col)) log2fc_col <- log2fc_col else log2fc_col <- "FC(log2)"
+  if (!is.null(fdr)) fdr <- fdr else fdr <- 0.05
+  if (!is.null(log2fc)) log2fc <- log2fc else log2fc <- 1
+  if (!is.null(TopHits)) TopHits <- TopHits else TopHits <- 10
+  
+  # data wrangling
+  res <- res %>%
+    mutate(`-log10padj` = -log10(!!sym(padj_col)))
+  
+  df_filt <- res %>%
+    mutate(`-log10padj` = -log10(!!sym(padj_col))) %>%
+    dplyr::filter(!!sym(padj_col) < fdr) %>%
+    mutate(status = case_when(!!sym(log2fc_col) < - log2fc ~ "Down",
+                              !!sym(log2fc_col) > log2fc ~ "Up") )
+  
+  
+  df_filt_up <- df_filt %>% dplyr::filter(status == "Up")
+  df_filt_down <- df_filt %>% dplyr::filter(status == "Down")
+  up_no <- nrow(dplyr::filter(df_filt, status == "Up"))
+  down_no <- nrow(dplyr::filter(df_filt, status == "Down"))
+  
+  ### truncate feature names if too long
+  df_filt <- df_filt %>%
+    mutate(across(!!sym(feature_col), ~ stringr::str_trunc(., width = 15, ellipsis = "")))
+  ### only add annotation to top 10 highlight
+  up_top10 <- df_filt_up %>%
+    arrange(!!sym(padj_col)) %>%
+    dplyr::slice(1:TopHits)
+  down_top10 <- df_filt_down %>%
+    arrange(!!sym(padj_col)) %>%
+    dplyr::slice(1:TopHits)
+  
+  # plot range parameter
+  volcano_xlim <- max(na.omit(abs(res[log2fc_col])))
+  volcano_ylim <- max(-log10(na.omit(res[padj_col])))
+  
+  # draw plot
+  feature_col <- sym(feature_col)
+  log2fc_col <- sym(log2fc_col)
+  ggplot(res, aes(!!log2fc_col, `-log10padj`)) +
+    geom_point(alpha = 0.4, size = 1.5, colour = "grey50", na.rm = TRUE) +
+    scale_x_continuous(limits = c(-volcano_xlim, volcano_xlim)) +
+    scale_y_continuous(limits = c(0, volcano_ylim)) +
+    
+    ### highlight up/down variables
+    geom_point(
+      data = df_filt_down, shape = 21, alpha = 0.6,
+      size = 1.5, fill = "blue", colour = "blue", na.rm = TRUE
+    ) +
+    geom_point(
+      data = df_filt_up, shape = 21, alpha = 0.6,
+      size = 1.5, fill = "red", colour = "red", na.rm = TRUE
+    ) +
+    ### add annotation to up/down variables
+    geom_point(
+      data = up_top10, shape = 21, fill = "red",
+      colour = "black", size = 2, na.rm = TRUE
+    ) +
+    ggrepel::geom_text_repel(
+      data = up_top10, aes(label = !!feature_col), na.rm = TRUE,
+      size = 4, max.overlaps = 20
+    ) +
+    geom_point(
+      data = down_top10, shape = 21, fill = "blue",
+      colour = "black", size = 2, na.rm = TRUE
+    ) +
+    ggrepel::geom_text_repel(
+      data = down_top10, aes(label = !!feature_col), na.rm = TRUE,
+      size = 4, max.overlaps = 20
+    ) +
+    ### add a frame to the plot, theme_bw
+    theme_bw(base_size = 14) +
+    ### add labels for x and y, and title to show highlight criteria
+    labs(
+      x = "log2-fold change",
+      y = "-log 10 (padj)",
+      title = paste("Volcano plot (", sprintf("FDR: %.2f, log2FC: %.1f", fdr, log2fc), ")", sep = ""),
+      subtitle = sprintf("Up %i Down %i", up_no, down_no)
+    )
+}
+
+volcano_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    sidebarPanel(
+      fluidRow(
+        column(4,
+               numericInput(NS(id,"PvlCut"),"P. Val Cutoff", value = 0.05)
+               ),
+        column(4,
+               numericInput(NS(id,"LFCCut"),"Log FC Cutoff", value = 1)
+               ),
+        column(4,
+               numericInput(NS(id,"TopHits"),"Top Hits", value = 10)
+        )
+      )
+    ),
+    mainPanel(
+      withSpinner(jqui_resizable(plotOutput(NS(id, "plot"))), type = 6),
+      downloadButton(NS(id, "dnld"), label = "")
+    )
+  )
+}
+
+volcano_server <- function(id, res, feature_col, padj_col, log2fc_col) {
+  moduleServer(id, function(input, output, session) {
+    
+    
+    plot <- reactive({
+      plotVolcano(res, feature_col, padj_col, log2fc_col, input$PvlCut, input$LFCCut, input$TopHits)
+    })
+    output$plot <- renderPlot({plot()})
+    output$dnld <- downloadHandler(
+      filename = function() {paste0("volcano", '.png')},
+      content = function(file) {ggsave(file, plot())}
+    )
+    
+  })
+}
+
+####----Volcano Enhanced----####
+
+volcanoEnh_ui <- function(id, GeneChoices) {
+  ns <- NS(id)
+  tagList(
+    sidebarPanel(
+      radioButtons(NS(id,"stat"), "Choose statistic for plot", 
+                   choices=c("-Log10 P value" = "P.Value",
+                             "-Log10 adjusted P value" = "adj.P.Val"), 
+                   selected = "P.Value", inline = T),
+      fluidRow(
+        column(6,
+               numericInput(NS(id,"Pvalue"), label = "Specify P-value", value = 0.05)
+        ),
+        column(6, 
+               numericInput(NS(id,"LogFC"), label = "Specify LogFC", value = 2)
+        )
+      ),
+      selectInput(NS(id,"VolGenesSel"), "Select Genes:", choices = GeneChoices, multiple = T)
+    ),
+    mainPanel(
+      withSpinner(jqui_resizable(plotOutput(NS(id, "volcanoEnh"), height = "550px", width = "800px")), type = 6),
+      downloadButton(NS(id,"dnld"), label = "")
+    )
+  )
+}
+
+volcanoEnh_server <- function(id, data) {
+  
+  moduleServer(id, function(input, output, session) {
+    
+    plot <- reactive({
+      
+      df <- data
+      
+      if (length(input$VolGenesSel) > 0) {
+        Features_Select <- input$VolGenesSel
+      } else Features_Select <- NULL
+      
+      VolPlot <- EnhancedVolcano(df,
+                                 lab = df[,1],
+                                 x = "logFC",
+                                 y = input$stat,
+                                 selectLab = Features_Select,
+                                 title = "Differential Expression",
+                                 subtitle = NULL,
+                                 FCcutoff = input$LogFC,
+                                 pCutoff = input$Pvalue,
+                                 legendPosition = "right",
+                                 legendLabSize = 12,
+                                 legendIconSize = 5.0,
+                                 drawConnectors = TRUE,
+                                 widthConnectors = 0.75)
+      VolPlot
+      
+    })
+    
+    output$volcanoEnh <- renderPlot(plot())
+    output$dnld <- downloadHandler(
+      filename = function() {paste0("volcano", '.png')},
+      content = function(file) {ggsave(file, plot())})
+    
+  })
+  
+}
+
+####----Mito Coverage----####
+
+mitoCov_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    sidebarPanel(
+      colourInput(NS(id,"curve_area"),"Pick coverage color", value = "#69b3a2"),
+      numericInput(NS(id,"bp_num"), "mtDNA bp value", value = 3243),
+      numericInput(NS(id,"cov_threshold"),"Threshold", value = 100)
+    ),
+    mainPanel(
+      withSpinner(jqui_resizable(plotOutput(NS(id,"mtCoverage"))), type = 6)
+    )
+  )
+}
+
+mitoCov_server <- function(id, data) {
+  moduleServer(id, function(input, output, session) {
+    output$mtCoverage <- renderPlot({
+      ggplot(combo, aes(x=position, y=mean, ymin=mean-sd, ymax=mean+sd)) +
+        geom_line() + geom_ribbon(alpha=0.2) +
+        geom_area(fill= input$curve_area, alpha=0.4) +
+        geom_hline(yintercept=input$cov_threshold,linetype=2)+
+        geom_point(aes(x=input$bp_num), shape = 95, size = 1, color = "red")+
+        theme(
+          panel.background = element_rect(fill="white", colour="white", size=0.5, 
+                                          linetype="solid", color="white"),
+          panel.border = element_blank(),
+          
+          #plot.title = element_text(color="black", size=14, face="bold.italic", hjust=0),
+          axis.title.x = element_text(color="black", size=14, face="plain"),
+          axis.title.y = element_text(color="black", size=14, face="plain"),
+          
+          legend.key=element_rect(fill='white'),
+          
+          axis.line = element_line(size = 0.5, linetype = "solid",colour = "black"),
+          axis.text.x = element_text(face="bold", color="black", 
+                                     size=12, angle=0),
+          axis.text.y = element_text(face="bold", color="black", 
+                                     size=12, angle=0))+
+        xlab("Mitochondria Position (bp)") +
+        ylab("Ave coverage") +
+        #labs(title = "CM 5primeGEX, scRNAseq PER CELL", subtitle = "Std Dev")+
+        scale_x_continuous(breaks=seq(0,16000,2000))+
+        scale_y_continuous(labels = function(x) format(x, scientific = FALSE))+
+        coord_cartesian(ylim = c(0,600000))
+    })
+  })
+}
+
+
+####----DMR Plot----####
+
+plotDMR <- function(data, coordinates, chrCol, startCol, endCol, intSamples, title = "") {
+  # get the sample names (column names of data matrix satisfying the following regular expression)
+  samples <- str_extract(colnames(data), "............_R0.C0.") # recognizes the sentrix ID format
+  samples <- samples[!is.na(samples)]
+  
+  # string split the given coordinates to pull the chromosome, start, and stop information
+  coordinates <- gsub(",", "", coordinates) # remove commas
+  chr <- strsplit(coordinates, split = ":")[[1]][1]
+  start <- strsplit(strsplit(coordinates, split = ":")[[1]][2], split = "-")[[1]][1]
+  stop <- strsplit(strsplit(coordinates, split = ":")[[1]][2], split = "-")[[1]][2]
+  
+  # acquire the row indexes of the data matrix that correspond to the start and stop coordinates
+  
+  # cut the data to match with chromosome specificity of the given coordinates
+  data <- data[data[, data[[chrCol]] == chr], ]
+  
+  # this is with the minimum distance from the given coordiantes to the data +1 / -1 probe
+  startPos <- which(abs(data[[startCol]] - as.numeric(start)) == min(abs(data[[startCol]] - as.numeric(start)))) - 1
+  endPos <- which(abs(data[[endCol]] - as.numeric(stop)) == min(abs(data[[endCol]] - as.numeric(stop)))) + 1
+  
+  # make sure that interest sample list is separated out from commas and remove spaces
+  intSamples <- gsub(" ", "", strsplit(intSamples, ",")[[1]])
+  
+  # pre-processing of data
+  data <- data %>%
+    slice(startPos:endPos) %>%
+    melt(id.vars = setdiff(colnames(data), samples), variable.name = "sampleID", value.name = "Beta") %>%
+    mutate(status = ifelse(sampleID %in% intSamples, "Sample-of-Interest", "normal"))
+  
+  # create the line plot
+  g <- ggplot(data = data, aes_string(x = startCol, y = "Beta")) +
+    geom_line(aes(group = sampleID, size = status, linetype = status, colour = status)) +
+    scale_colour_manual(values = c("Sample-of-Interest" = "red", "normal" = "black")) +
+    scale_size_manual(values = c("Sample-of-Interest" = 0.9, "normal" = 0.2)) +
+    scale_linetype_manual(values = c("Sample-of-Interest" = "solid", "normal" = "dashed")) +
+    coord_cartesian(ylim = c(0, 1)) +
+    xlab(paste0("Genomic Position at ", chr)) +
+    ylab("Methylation Value") +
+    labs(title = title) +
+    theme_bw()
+  g
+}
+
+plotDMR_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    sidebarPanel(
+      textInput(NS(id, "coordinates"), "What genomic coordinates would you like to view? Please use format chrX:start-stop ", value = "chr16:17562418-17565190"),
+      textInput(NS(id, "chrCol"), "What is the name of the column with the chromosome information? (e.g. chr16)", value = "CpG_chrm"),
+      textInput(NS(id, "startCol"), "What is the name of the column with the start positions? (e.g. 17562418)", value = "CpG_beg"),
+      textInput(NS(id, "endCol"), "What is the name of the column with the end positions? (e.g. 17565190)", value = "CpG_end"),
+      textInput(NS(id, "intSamples"), "Are there samples of interest to highlight (e.g. 203866380012_R06C01, 204873630063_R08C01)?", value = "203866380012_R06C01, 204873630063_R08C01"),
+      textInput(NS(id, "title"), "What is the title of the plot?", value = "DMR plot at XYLT1"),
+    ),
+    mainPanel(
+      titlePanel("Interactive DMR plot"),
+      verbatimTextOutput(NS(id, "region")),
+      p(),
+      withSpinner(jqui_resizable(plotOutput(NS(id, "plot"))), type = 6)
+    )
+  )
+}
+
+plotDMR_server <- function(id, data) {
+  moduleServer(id, function(input, output, session) {
+    output$region <- renderText(paste0("The region you have selected is: ", input$coordinates, " and the samples of interest are: ", input$intSamples))
+    
+    DMR_plot <- reactive({
+      plotDMR(data, input$coordinates, input$chrCol, input$startCol, input$endCol, input$intSamples, title = input$title)
+    })
+    
+    output$plot <- renderPlot({
+      DMR_plot()
+    })
+    
+  })
+}
+
+####----UI and Server----####
+
+ui <- 
+  navbarPage("Shiny Module Demos",
+             tabPanel("Volcano",
+                      fluidPage(
+                        mainPanel(
+                          module_volcano_ui("volcano")
+                        )
+                      )
+             ),
+             tabPanel("Volcano 2",
+                      fluidPage(
+                        mainPanel(
+                          volcano_ui("x")
+                        )
+                      )
+             ),
+             tabPanel("Enhanced Volcano",
+                      fluidPage(
+                        mainPanel(
+                          volcanoEnh_ui("VolcanoEnh", GeneChoices = volGenes)
+                        )
+                      )
+             ),
+             tabPanel("Mito Coverage",
+                      fluidPage(
+                        mainPanel(
+                          mitoCov_ui("mtCoverage")
+                        )
+                      )
+             ),
+             tabPanel("DMR Plot",
+                      fluidPage(
+                        mainPanel(
+                          plotDMR_ui("DMRmethPlot")
+                        )
+                      )
+             )
+  )
+
+
+server <- function(input, output, session) {
+  module_volcano_server("volcano", data = L29_Vitro_Diff)
+  volcano_server("x", L29_Vitro_Diff, feature_col = "gene",
+                 padj_col = "adj.P.Val", log2fc_col = "logFC")
+  
+  volcanoEnh_server("VolcanoEnh",data = L29_Vitro_Diff)
+  mitoCov_server("mtCoverage",combo)
+  plotDMR_server("DMRmethPlot",DMR_data)
+}
+
+
+shinyApp(ui, server)
+
+
