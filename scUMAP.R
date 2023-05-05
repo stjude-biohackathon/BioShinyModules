c18b4e611e83e6a80138cbb73e3818c680d53202
Louis <louislenezet@gmail.com>
2023-05-05 15:20:35 +0200

    scUMAP

---
+++ scUMAP.R
@@ -0,0 +1,78 @@
+# This module was created during the St Jude Bio-Hackathon of May 2023 by the team 13.
+# author: @KMcC73
+# author: Louis Le Nézet (louislenezet@gmail.com)
+
+# Documentation
+#' R Shiny module to generate XXX graph
+#'
+#' @param id A string.
+#' @param df A dataframe.
+#' @returns A Shiny module.
+#' @examples
+#' scUMAP_demo()
+
+#### Library needed #### ----------
+library(Seurat)
+library(shiny)
+
+#### Function needed to work #### ----------
+# Documentation
+#' Function that process the data
+#'
+#' @param df A dataframe.
+#' @returns A process data.
+
+process_df <- function(df, resolution=0.5) {
+  df <- NormalizeData(df)
+  df <- FindVariableFeatures(df, selection.method = "vst", nfeatures = 3000)
+  df <- ScaleData(df, verbose = FALSE)
+  df <- RunPCA(df, npcs = 15, verbose = FALSE)
+  df <- RunUMAP(df, reduction = "pca", dims = 1:15)
+  df <- FindNeighbors(df, reduction = "pca", dims = 1:15)
+  df <- RunTSNE(df)
+  df <- FindClusters(df, resolution = resolution)
+  return(df)
+}
+
+
+#### UI function of the module #### ----------
+scUMAP_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    numericInput(ns("parameter"), "Select Resolution", value = 0.5),
+    plotOutput(ns("UMAP"))
+  )
+}
+
+#### Server function of the module #### ----------
+scUMAP_server <- function(id, df) {
+  stopifnot(is.reactive(df))
+
+  moduleServer(id, function(input, output, session) {
+    output$UMAP <- renderPlot({
+      # Processing data
+      df_processed <- process_df(df(), input$parameter)
+
+      # UMAP
+      DimPlot(df_processed, reduction = "umap", cols = ggsci::pal_igv()(50))
+    })
+  })
+}
+
+#### Demo function of the module #### ----------
+scUMAP_demo <- function() {
+  df <- readRDS("./example_data/pbmc3k.rds")
+  ui <- fluidPage(
+    scUMAP_ui("UMAP")
+  )
+  server <- function(input, output, session) {
+    scUMAP_server("UMAP", reactive({
+      df
+    }))
+  }
+  shinyApp(ui, server)
+}
+
+scUMAP_demo()
+
+# TODO add documentation
