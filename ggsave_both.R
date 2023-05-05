3ad16e8c1726045e514302184740af5aef6d551f
Louis <louislenezet@gmail.com>
2023-05-05 15:21:38 +0200

    ggsaveboth

--- ggsave_both.R
+++ ggsave_both.R
@@ -5,88 +5,70 @@
 #' R Shiny module to save a plot to file, choosing between png and pdf
 #'
 #' @param id A string.
-#' @param df A dataframe.
+#' @param plot A plot to save.
 #' @returns A Shiny module.
 #' @examples
-#' module.demo()
-
+#' ggsaveBoth_demo()
 #### Library needed #### ----------
 library(ggplot2)
 library(shiny)
 
-#### Function needed to work #### ----------
-
 
 #### UI function of the module #### ----------
-
 ggsaveBoth_ui <- function(id) {
-
-        tagList(
-                numericInput( NS(id, "var1"), "Figure width", value = 15, min = 0, max = 20),
-                numericInput( NS(id, "var2"), "Figure height", value = 10, min = 0, max = 20),
-                radioButtons( NS(id, "var3"), label = "Select the file type", choices = list("png", "pdf"), selected = "png"),
-                downloadButton( NS(id, "dnld"), label = "")
-                )
+  ns <- NS(id)
+  tagList(
+    numericInput(ns("var1"), "Figure width", value = 15, min = 0, max = 20),
+    numericInput(ns("var2"), "Figure height", value = 10, min = 0, max = 20),
+    radioButtons(ns("var3"), label = "Select the file type", choices = list("png", "pdf"), selected = "png"),
+    downloadButton(ns("dnld"), label = "")
+  )
 }
 
 #### Server function of the module #### ----------
 
-ggsaveBoth_server <- function(id, plot) {
+ggsaveBoth_server <- function(id, my_plot) {
+  stopifnot(is.reactive(my_plot))
 
-        stopifnot(is.reactive(plot))
-
-        moduleServer(id, function(input, output, session) {
-
-                output$dnld <- downloadHandler(
-
-                        filename =  function() {paste("saveplot", input$var3, sep=".")},
-                        content = function(file) {
-                                ggsave(file, plot(), width = input$var1, height = input$var2, device = input$var3)
-                                # if(input$var3 == "png")
-                                #         png(file) # open the png device
-                                # else
-                                #         pdf(file) # open the pdf device
-                                # dev.off()
-                        }
-                )
-
-        })
+  moduleServer(id, function(input, output, session) {
+    output$dnld <- downloadHandler(
+      filename = function() {
+        paste("saveplot", input$var3, sep = ".")
+      },
+      content = function(file) {
+        ggsave(filename = file, plot = my_plot(), width = input$var1, height = input$var2, device = input$var3)
+        # if(input$var3 == "png")
+        #         png(file) # open the png device
+        # else
+        #         pdf(file) # open the pdf device
+        # dev.off()
+        print("Test2")
+      }
+    )
+  })
 }
 
-#### Demo functionof the module #### ----------
-load("./example_data/MS_2.rda")
-source("plotHist.R")
-
+#### Demo function of the module #### ----------
 ggsaveBoth_demo <- function() {
+  source("plotHist.R")
+  source("dataImport.R")
+  ui <- fluidPage(
+    dataImport_ui("datafile", "User data"),
+    plotHist_ui("hist"),
+    ggsaveBoth_ui("savehist")
+  )
 
-        ui <- fluidPage(
-                csvImport_ui("datafile", "User data (.csv format)"),
-                histogram_ui("hist"),
-                ggsaveBoth_ui("savehist")
-                )
+  server <- function(input, output, session) {
+    data <- dataImport_server("datafile")
+    df <- reactive({
+      data() %>%
+        unlist()
+    })
+    my_plot <- plotHist_server("hist", df)
 
-        server <- function(input, output, session) {
-                data <- csvImport_server("datafile", stringsAsFactors = TRUE)
-
-                df <- reactive({
-                        data() %>%
-                                unlist()
-                })
-
-                plot = histogram_server("hist", df)
-
-                getplot = reactive({
-                        plot()
-                })
-                ggsaveBoth_server("savehist", getplot )
-        }
-        shinyApp(ui, server)
+    ggsaveBoth_server("savehist", my_plot)
+  }
+  shinyApp(ui, server)
 }
 
-
-
-ggsaveBoth_demo()
-# TODO list for this template
-# TODO rename all the function as modulename_function
-# TODO add minimal data for testing
-# TODO update documentation
+# TODO rename the function
