373f328cc173fdfea98d5774d674abb2ed969432
LouisBzh <louislenezet@gmail.com>
2023-06-01 12:05:10 +0200

    Data reorganisation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' DataName
+#'
+#' @description
+#'
+#' @details
+#'
+#' @format A data frame with nine variables:
+#' \describe{
+#' \item{\code{var1}}{var1 descritpion}
+#' \item{\code{var2}}{var2 descritpion}
+#' }
+#'
+#' For further details, see \url{dataurl}
+#'
+"DataName"
---
+++ data-raw/files-to-rda.R
@@ -0,0 +1,9 @@
+# All the files present in data-raw need to be
+# transferred to the data folder as rda
+
+# Here is written the script to do so
+
+# The best would be:
+# - source the original file
+# - modify it here according to the needs
+# - export it to the data folder as rda
