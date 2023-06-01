d8becf6ebc44f75a5476c7fda91d74f2509e4915
LouisBzh <louislenezet@gmail.com>
2023-06-01 12:06:47 +0200

    Add new test for data import functions

--- tests/testthat/test-dataImport.R
+++ tests/testthat/test-dataImport.R
@@ -1,4 +1,19 @@
 test_that("read_data works", {
-  df <- read_data("/data/Limma.csv", sep=",")
-  expect_equal(dim(df),c(4,500))
+  f_path <- file.path(system.file("data-raw", package = "BioShinyModules"), "Limma.csv")
+  df <- read_data(f_path, sep = ",")
+  expect_equal(dim(df), c(15095, 7))
+
+  f_path <- file.path(system.file("data-raw", package = "BioShinyModules"), "MS_2.rda")
+  df <- read_data(f_path, sep = ",", df_name = "df")
+  expect_equal(dim(df), c(164, 32))
+})
+
+test_that("get_dataframe works", {
+  f_path <- file.path(system.file("data-raw", package = "BioShinyModules"), "Limma.csv")
+  df_name <- get_dataframe(f_path)
+  expect_equal(df_name, NULL)
+
+  f_path <- file.path(system.file("data-raw", package = "BioShinyModules"), "MS_2.rda")
+  df_name <- get_dataframe(f_path)
+  expect_equal(df_name, c("df", "feature_meta", "sample_meta"))
 })
