768f7e48cdb536b2b555666256881b423015c8a0
whats-in-the-box <ytqiuhaowen@gmail.com>
2023-06-08 16:46:43 -0500

    Finalized plotHeatmaply for heatmap/distance/correlation plots

--- R/modules/plotHeatmaply.R
+++ R/modules/plotHeatmaply.R
@@ -6,10 +6,11 @@
 #' R Shiny module to generate a heatmap/distance/correlation using `heatmaply`
 #'
 #' @param id A string.
-#' @param df A dataframe containing the value for each sample (column) for all the features (rows).
+#' @param df A dataframe.
 #' @returns A Shiny module.
 #' @examples
 #' plotHeatmaply_demo()
+#' plotHeatmaply_demo_2()
 #### Library needed #### ----------
 library(shiny)
 library(heatmaply)
@@ -18,7 +19,19 @@ library(RColorBrewer)
 #### Function needed to work #### ----------
 #' plot Heatmap
 #'
-#' @param df A data frame with feature (row) by sample (column)
+#' @param df A data frame
+#' @param type Character, choose from one of three values for plot types
+#' @param direction
+#' @param select_dist Character
+#' @param select_corr Character
+#' @param select_scale Character
+#' @param draw_dendrogram Character
+#' @param showRowDendrogram Boolean
+#' @param showColDendrogram Boolean
+#' @param showRowLabel Boolean
+#' @param showColLabel Boolean
+#' @param pal Character
+#' @param title Character
 #'
 #' @return A heatmap plot generated with pheatmap
 #' @export
@@ -29,26 +42,30 @@ plotHeatmaply <- function(df, type = c("heatmap", "correlation", "distance"),
                                              c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski"), NULL),
                         select_corr = ifelse(type == "correlation",
                                              c("pearson", "kendall", "spearman"), NULL),
-                        #select_hclust = c("ward.D", "ward.D2", "single", "complete", "average",
-                        #                   "mcquitty", "median", "centroid"),
-                        select_scale = ifelse(type == "heatmap", c("column", "row"), NULL ),
+                        select_hclust = c("ward.D", "ward.D2", "single", "complete", "average",
+                                           "mcquitty", "median", "centroid"),
+                        select_scale = ifelse(type == "heatmap", c("column", "row"), "none" ),
                         draw_dendrogram = c('none', 'row', 'column', 'both' ),
                         showRowDendrogram = TRUE,
                         showColDendrogram = TRUE,
                         showRowLabel = TRUE,
                         showColLabel = TRUE,
+                        fontsize_col,
+                        fontsize_row,
                         pal,
                         title
                         ) {
 
+        df = data.frame(df)
+
         if (type == "heatmap"){
 
-                cat("When 'type' == 'heatmap', 'direction', 'select_dist', 'select_corr' are set at NULL. ")
+                cat("When 'type' == 'heatmap', 'direction', 'select_dist', 'select_corr' are set at NULL. \n ")
                 mat = as.matrix(df)
 
         } else if (type == "correlation"){
 
-                cat("When 'type' == 'correlation', 'select_scale' is set at 'none' and 'select_dist' is set at NULL. ")
+                cat("When 'type' == 'correlation', 'select_scale' is set at 'none' and 'select_dist' is set at NULL. \n ")
 
                 if (direction == "row"){
                         mat = cor(t(df), method = select_corr)
@@ -60,7 +77,7 @@ plotHeatmaply <- function(df, type = c("heatmap", "correlation", "distance"),
                 }
         } else if (type == "distance"){
 
-                cat("When 'type' == 'distance', 'select_scale' is set at 'none', and 'select_corr' is set at NULL. ")
+                cat("When 'type' == 'distance', 'select_scale' is set at 'none', and 'select_corr' is set at NULL. \n")
 
                 if (direction == "row"){
                         mat = as.matrix(dist(df, method = select_dist))
@@ -73,28 +90,27 @@ plotHeatmaply <- function(df, type = c("heatmap", "correlation", "distance"),
 
 
         # select colors for hm and anno
-        hm_color <- colorRampPalette(brewer.pal(n = 11, name = pal))(256)
+        hm_color <- colorRampPalette(brewer.pal(n = palettes$maxcolors[rownames(palettes) == pal], name = pal))(256)
 
         # draw hm
         heatmaply( mat,
-                   colors = colorRampPalette(rev(RColorBrewer::brewer.pal(n = 8, name = pal)))(200),
+                   colors = hm_color,
                    scale = select_scale,
                    dendrogram = draw_dendrogram,
                    show_dendrogram = c(showRowDendrogram, showColDendrogram),
                    dist_method = NULL,
-                   hclust_method = NULL,
+                   hclust_method = select_hclust,
                    showticklabels = c(showRowLabel, showColLabel),
+                   fontsize_col = fontsize_col,
+                   fontsize_row = fontsize_row,
                    main = title
         )
 }
 
 palettes <- RColorBrewer::brewer.pal.info
-hm_palettes <- rownames(palettes[which(palettes$category == "div"),])
-dist_palettes <- rownames(palettes[which(palettes$category == "seq"),])
-#dist = c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")
-#corr = c("pearson", "kendall", "spearman")
-#scale = c("none", "row", "column")
-#dendrogram = c('none', 'row', 'column', 'both' )
+hm_palettes <- rownames(palettes[palettes$category == "div",])
+dist_palettes <- rownames(palettes[palettes$category == "seq",])
+
 
 #### UI function of the module #### ----------
 plotHeatmaply_ui <- function(id) {
@@ -119,9 +135,9 @@ plotHeatmaply_ui <- function(id) {
                         condition = sprintf("input['%s'] == 'distance'", ns("plot_type")),
                                 #"input.plot_type == 'distance'",
                         selectInput(ns("dist_palette"), "Choose color palette",
-                                    choices = dist_palettes, selected = "Blues"
+                                    choices = dist_palettes, selected = "YlGnBu"
                         ),
-                        selectInput(ns("direction"), "Select direction",
+                        selectInput(ns("dist_direction"), "Select direction",
                                     choices = c("col", "row"), selected = "col"
                         ),
                         selectInput(ns("dist"), "Select distance metric",
@@ -135,9 +151,9 @@ plotHeatmaply_ui <- function(id) {
                         condition = sprintf("input['%s'] == 'correlation'", ns("plot_type")),
                                 #"input.plot_type == 'correlation'",
                         selectInput(ns("corr_palette"), "Choose color palette",
-                                    choices = hm_palettes, selected = "RdYlBu"
+                                    choices = hm_palettes, selected = "RdBu"
                         ),
-                        selectInput(ns("direction"), "Select direction",
+                        selectInput(ns("corr_direction"), "Select direction",
                                     choices = c("col", "row"), selected = "col"
                         ),
                         selectInput(ns("corr"), "Select correlation method",
@@ -145,6 +161,11 @@ plotHeatmaply_ui <- function(id) {
                                     selected = "pearson"
                         )
 
+                ),
+                selectInput(ns("select_hclust"), "Select hclust method",
+                            choices = c("ward.D", "ward.D2", "single", "complete", "average",
+                                        "mcquitty", "median", "centroid"),
+                            selected = "complete"
                 ),
 
                 selectInput(ns("dendrogram"), "Draw Dendrogram",
@@ -154,6 +175,10 @@ plotHeatmaply_ui <- function(id) {
                 checkboxInput(ns("showColDendrogram"), "Show col dendrogram", value = TRUE),
                 checkboxInput(ns("showRowLabel"), "Show row label", value = TRUE),
                 checkboxInput(ns("showColLabel"), "Show col label", value = TRUE),
+                sliderInput(ns("fontsize_col"),"Select font size for column label",
+                            min = 5, max = 15, value = 8),
+                sliderInput(ns("fontsize_row"),"Select font size for row label",
+                            min = 5, max = 15, value = 8),
                 textInput(ns("title"), "Title of the graph", value = ""),
                 actionButton( ns("click_submit"), label = "Submit" ),
                 plotOutput(ns("plot"))
@@ -173,37 +198,46 @@ plotHeatmaply_server <- function(id, df) {
                                 plotHeatmaply(df(),
                                               type = input$plot_type,
                                               select_scale = input$scale,
+                                              select_hclust = input$select_hclust,
                                               draw_dendrogram = input$dendrogram,
                                               showRowDendrogram = input$showRowDendrogram,
                                               showColDendrogram = input$showColDendrogram,
                                               showRowLabel = input$showRowLabel,
                                               showColLabel = input$showColLabel,
+                                              fontsize_col = input$fontsize_col,
+                                              fontsize_row = input$fontsize_row,
                                               pal = input$hm_palette,
                                               title = input$title
                                       )
                         } else if (input$plot_type == "distance"){
                                 plotHeatmaply(df(),
                                               type = input$plot_type,
-                                              direction = input$direction,
+                                              direction = input$dist_direction,
                                               select_dist = input$dist,
+                                              select_hclust = input$select_hclust,
                                               draw_dendrogram = input$dendrogram,
                                               showRowDendrogram = input$showRowDendrogram,
                                               showColDendrogram = input$showColDendrogram,
                                               showRowLabel = input$showRowLabel,
                                               showColLabel = input$showColLabel,
+                                              fontsize_col = input$fontsize_col,
+                                              fontsize_row = input$fontsize_row,
                                               pal = input$dist_palette,
                                               title = input$title
                                 )
                         } else if (input$plot_type == "correlation"){
                                 plotHeatmaply(df(),
                                               type = input$plot_type,
-                                              direction = input$direction,
+                                              direction = input$corr_direction,
                                               select_corr = input$corr,
+                                              select_hclust = input$select_hclust,
                                               draw_dendrogram = input$dendrogram,
                                               showRowDendrogram = input$showRowDendrogram,
                                               showColDendrogram = input$showColDendrogram,
                                               showRowLabel = input$showRowLabel,
                                               showColLabel = input$showColLabel,
+                                              fontsize_col = input$fontsize_col,
+                                              fontsize_row = input$fontsize_row,
                                               pal = input$corr_palette,
                                               title = input$title
                                 )
@@ -223,12 +257,36 @@ plotHeatmap_demo <- function() {
         load("../../data-raw/MS_2.rda")
         df <- df
 
-        ui <- fluidPage(plotHeatmaply_ui("plotHeatmaply"))
+        ui <- fluidPage(
+                plotHeatmaply_ui("plotHeatmaply")
+        )
+
         server <- function(input, output, session) {
 
-                plotHeatmaply_server("plotHeatmaply",
+                my_plot <- plotHeatmaply_server("plotHeatmaply",
                                      reactive({df})
                 )
+        }
+        shinyApp(ui, server)
+}
+
+
+plotHeatmap_demo_2 <- function() {
+        source("../exportPlot.R")
+        load("../../data-raw/MS_2.rda")
+        df <- df
+
+        ui <- fluidPage(
+                plotHeatmaply_ui("plotHeatmaply"),
+                exportPlot_ui("savepca")
+        )
+
+        server <- function(input, output, session) {
+
+                my_plot <- plotHeatmaply_server("plotHeatmaply",
+                                     reactive({df})
+                                     )
+                exportPlot_server("savepca", my_plot, is_plotly = TRUE)
         }
         shinyApp(ui, server)
 }
