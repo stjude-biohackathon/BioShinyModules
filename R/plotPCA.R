ab026e9c738478cdff3e8d54b8e25114bb37e3ca
LouisBzh <louislenezet@gmail.com>
2023-06-13 10:43:51 +0200

    Check plotPCA

--- R/plotPCA.R
+++ R/plotPCA.R
@@ -14,13 +14,12 @@
 #' @examples
 #' plotPCA_demo()
 #' plotPCA_demo_2()
-#'
 #### Library needed #### ----------
-library(shiny)
-library(ggplot2)
-library(pcaMethods)
-library(plotly)
-library(RColorBrewer)
+usethis::use_package("shiny")
+usethis::use_package("ggplot2")
+usethis::use_package("pcaMethods")
+usethis::use_package("plotly")
+usethis::use_package("RColorBrewer")
 
 #### Function needed to work #### ----------
 #' plot PCA 2D
@@ -31,29 +30,31 @@ library(RColorBrewer)
 #' @param title Character. Plot title
 #'
 #' @return GGplot PCA
+#' @import ggplot2
+#' @import pcaMethods
 ggplotly_2Dpca <- function(df, sample_anno, sample_anno_col, PC1, PC2, pal, title, font_size) {
-    require(pcaMethods)
-    require(ggplot2)
+  requireNamespace("pcaMethods")
+  requireNamespace("ggplot2")
 
-    data <- as.matrix(df)
-    class(data) <- "numeric"
+  data <- as.matrix(df)
+  class(data) <- "numeric"
 
-    labels <- as.matrix(sample_anno[sample_anno_col])
+  labels <- as.matrix(sample_anno[sample_anno_col])
 
-    pc1 <- pcaMethods::pca(t(data), nPcs = 3, scale = "pareto")
-    pc1merged <- merge(cbind(labels, t(data)),
-        pcaMethods::scores(pc1),
-        by = 0
-    )
-    p <- ggplot(pc1merged, aes(!!sym(PC1), !!sym(PC2), colour = !!sym(sample_anno_col))) +
-            geom_point() +
-            scale_color_brewer(palette = pal) +
-            stat_ellipse() +
-            xlab(paste(PC1, round((pc1@R2[PC1] * 100), digits = 1), "% of the variance")) +
-            ylab(paste(PC2, round((pc1@R2[PC2] * 100), digits = 1), "% of the variance")) +
-            ggtitle(label = title) +
-            theme(text = element_text(size = as.numeric(font_size)))
-    ggplotly(p)
+  pc1 <- pcaMethods::pca(t(data), nPcs = 3, scale = "pareto")
+  pc1merged <- merge(cbind(labels, t(data)),
+    pcaMethods::scores(pc1),
+    by = 0
+  )
+  p <- ggplot(pc1merged, aes(!!sym(PC1), !!sym(PC2), colour = !!sym(sample_anno_col))) +
+    geom_point() +
+    scale_color_brewer(palette = pal) +
+    stat_ellipse() +
+    xlab(paste(PC1, round((pc1@R2[PC1] * 100), digits = 1), "% of the variance")) +
+    ylab(paste(PC2, round((pc1@R2[PC2] * 100), digits = 1), "% of the variance")) +
+    ggtitle(label = title) +
+    theme(text = element_text(size = as.numeric(font_size)))
+  ggplotly(p)
 }
 
 #' Plot PCA 3D
@@ -64,137 +65,145 @@ ggplotly_2Dpca <- function(df, sample_anno, sample_anno_col, PC1, PC2, pal, titl
 #' @param title Character. Plot title
 #'
 #' @return Interactive PCA plot
+#' @import pcaMethods
+#' @import plotly
 ggplotly_3Dpca <- function(df, sample_anno, sample_anno_col, pal, title) {
-    data <- as.matrix(df)
-    class(data) <- "numeric"
+  data <- as.matrix(df)
+  class(data) <- "numeric"
 
-    labels <- as.matrix(sample_anno[sample_anno_col])
+  labels <- as.matrix(sample_anno[sample_anno_col])
 
-    pc1 <- pcaMethods::pca(t(data), nPcs = 3, scale = "pareto")
-    pc1merged <- merge(cbind(labels, t(data)),
-        pcaMethods::scores(pc1),
-        by = 0
+  pc1 <- pcaMethods::pca(t(data), nPcs = 3, scale = "pareto")
+  pc1merged <- merge(cbind(labels, t(data)),
+    pcaMethods::scores(pc1),
+    by = 0
+  )
+
+  plot_ly(pc1merged,
+    x = ~PC1, y = ~PC2, z = ~PC3, type = "scatter3d",
+    color = sample_anno$sampleLabel, colors = pal,
+    text = sample_anno$sampleName, hoverinfo = "text"
+  ) %>%
+    layout(
+      title = list(title),
+      scene = list(
+        xaxis = list(title = "PC1"),
+        yaxis = list(title = "PC2"),
+        zaxis = list(title = "PC3")
+      )
     )
-
-    plot_ly(pc1merged,
-        x = ~PC1, y = ~PC2, z = ~PC3, type = "scatter3d",
-        color = sample_anno$sampleLabel, colors = pal,
-        text = sample_anno$sampleName, hoverinfo = "text"
-    ) %>%
-        layout(title = list(title ),
-               scene = list(
-                       xaxis = list(title = "PC1" ),
-                       yaxis = list(title = "PC2" ),
-                       zaxis = list(title = "PC3" )
-                       )
-               )
 }
 
 palettes <- rownames(RColorBrewer::brewer.pal.info)
 
 #### UI function of the module #### ----------
+# TODO add documentation
+#' @import shiny
 plotPCA_ui <- function(id) {
-    ns <- NS(id)
-    tagList(
-        selectInput(ns("palette"), "Choose color palette", choices = palettes, selected = "Dark2"),
-        textInput(ns("title"), "Title graph", value = ""),
-        radioButtons(ns("dim_select"), "Select 2D or 3D",
-            choices = c("2D", "3D"), selected = "2D"
-        ),
-        selectInput(ns("PC1"), "Select first PC",
-            choices = list("PC1", "PC2", "PC3"),
-            selected = "PC1"
-        ),
-        selectInput(ns("PC2"), "Select second PC",
-            choices = list("PC1", "PC2", "PC3"),
-            selected = "PC2"
-        ),
-        sliderInput(ns("fontSize"), "Select font size",
-                    min = 8, max = 18, value = 11
-        ),
-        plotlyOutput(ns("plot"))
-    )
+  ns <- NS(id)
+  tagList(
+    selectInput(ns("palette"), "Choose color palette", choices = palettes, selected = "Dark2"),
+    textInput(ns("title"), "Title graph", value = ""),
+    radioButtons(ns("dim_select"), "Select 2D or 3D",
+      choices = c("2D", "3D"), selected = "2D"
+    ),
+    selectInput(ns("PC1"), "Select first PC",
+      choices = list("PC1", "PC2", "PC3"),
+      selected = "PC1"
+    ),
+    selectInput(ns("PC2"), "Select second PC",
+      choices = list("PC1", "PC2", "PC3"),
+      selected = "PC2"
+    ),
+    sliderInput(ns("fontSize"), "Select font size",
+      min = 8, max = 18, value = 11
+    ),
+    plotlyOutput(ns("plot"))
+  )
 }
 
 #### Server function of the module #### ----------
+# TODO add documentation
+#' @import shiny
 plotPCA_server <- function(id, df, sample_anno, sample_anno_col) {
-    moduleServer(id, function(input, output, session) {
-        stopifnot(is.reactive(df))
-        stopifnot(is.reactive(sample_anno))
-        stopifnot(is.reactive(sample_anno_col))
+  moduleServer(id, function(input, output, session) {
+    stopifnot(is.reactive(df))
+    stopifnot(is.reactive(sample_anno))
+    stopifnot(is.reactive(sample_anno_col))
 
-        fontSize <- reactive({
-                input$fontSize
-        })
-
-        PCA_plot <- reactive({
-            if (input$dim_select == "2D") {
-                ggplotly_2Dpca(
-                        df(), sample_anno(), sample_anno_col(),
-                        input$PC1, input$PC2,
-                        input$palette, input$title, fontSize()
-                        )
-            } else {
-                ggplotly_3Dpca(
-                        df(), sample_anno(), sample_anno_col(),
-                        input$palette, input$title
-                        )
-            }
-        })
-
-        output$plot <- renderPlotly({
-            PCA_plot()
-        })
-
-        return(PCA_plot)
+    fontSize <- reactive({
+      input$fontSize
     })
+
+    PCA_plot <- reactive({
+      if (input$dim_select == "2D") {
+        ggplotly_2Dpca(
+          df(), sample_anno(), sample_anno_col(),
+          input$PC1, input$PC2,
+          input$palette, input$title, fontSize()
+        )
+      } else {
+        ggplotly_3Dpca(
+          df(), sample_anno(), sample_anno_col(),
+          input$palette, input$title
+        )
+      }
+    })
+
+    output$plot <- renderPlotly({
+      PCA_plot()
+    })
+
+    return(PCA_plot)
+  })
 }
 
 #### Demo function of the module #### ----------
-
+# TODO add documentation
 plotPCA_demo <- function() {
-    load("../../data-raw/MS_2.rda")
-    df <- df
-    sample_anno <- sample_meta
-    sample_anno_col <- "sampleLabel"
+  load("../../data-raw/MS_2.rda")
+  df <- df
+  sample_anno <- sample_meta
+  sample_anno_col <- "sampleLabel"
 
-    ui <- fluidPage(plotPCA_ui("x"))
+  ui <- fluidPage(plotPCA_ui("x"))
 
-    server <- function(input, output, session) {
-        my_plot <- plotPCA_server("x", reactive({
-            df
-        }), reactive({
-            sample_anno
-        }), reactive({
-            sample_anno_col
-        }))
-    }
-    shinyApp(ui, server)
+  server <- function(input, output, session) {
+    my_plot <- plotPCA_server("x", reactive({
+      df
+    }), reactive({
+      sample_anno
+    }), reactive({
+      sample_anno_col
+    }))
+  }
+  shinyApp(ui, server)
 }
 
-# to-do: download using exportPlot()
+# TODO add documentation
+# TODO: download using exportPlot()
 # htmo download works for 3D plot, png and pdf needs to work for 2D plot
 plotPCA_demo_2 <- function() {
-    source("exportPlot.R")
-    load("../../data-raw/MS_2.rda")
-    df <- df
-    sample_anno <- sample_meta
-    sample_anno_col <- "sampleLabel"
+  source("exportPlot.R")
+  load("../../data-raw/MS_2.rda")
+  df <- df
+  sample_anno <- sample_meta
+  sample_anno_col <- "sampleLabel"
 
-    ui <- fluidPage(
-        plotPCA_ui("x"),
-        exportPlot_ui("savepca")
-    )
+  ui <- fluidPage(
+    plotPCA_ui("x"),
+    exportPlot_ui("savepca")
+  )
 
-    server <- function(input, output, session) {
-        my_plot <- plotPCA_server("x", reactive({
-            df
-        }), reactive({
-            sample_anno
-        }), reactive({
-            sample_anno_col
-        }))
-        exportPlot_server("savepca", my_plot, is_plotly = TRUE)
-    }
-    shinyApp(ui, server)
+  server <- function(input, output, session) {
+    my_plot <- plotPCA_server("x", reactive({
+      df
+    }), reactive({
+      sample_anno
+    }), reactive({
+      sample_anno_col
+    }))
+    exportPlot_server("savepca", my_plot, is_plotly = TRUE)
+  }
+  shinyApp(ui, server)
 }
